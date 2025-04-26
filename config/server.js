16bdaedacee74aadeaa02a94fb78447e6fb36172
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-04-26 15:19:07 -0700

    v3

--- config/server.js
+++ config/server.js
@@ -1,21 +1,10 @@
-const strapi = require('strapi');
-const http = require('http');
-
-module.exports = ({ env }) => {
-  const app = strapi();
-  
-  app
-    .load()
-    .then(() => {
-      const server = http.createServer(app.callback());
-      
-      // Listen on the given port (Vercel will set the PORT automatically)
-      server.listen(env.int('PORT', 1337), '0.0.0.0', () => {
-        console.log('Strapi is running...');
-      });
-    })
-    .catch((err) => {
-      console.error('Error loading Strapi:', err);
-      process.exit(1);
-    });
-};
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
